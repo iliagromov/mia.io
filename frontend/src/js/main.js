90c2dfd15e428dafddba7b8232e37ddeb44eff29
iliagromov <ilia.workproffile@gmail.com>
2023-07-07 23:17:27 +0300

    added from

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -5,8 +5,10 @@ const { createVuetify } = Vuetify;
 const vuetify = createVuetify();
 
 const app = createApp({
+    el: "#app",
     data: () => ({
         items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
     }),
+    ex3: 'primary',
 })
 app.use(vuetify).mount('#app');
