7295f1567833c5adcc9a47943ef8f1e0d9ab0b70
iliagromov <ilia.workproffile@gmail.com>
2023-07-10 05:14:41 +0300

    Сделал правки

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -5,53 +5,79 @@ const { createVuetify } = Vuetify;
 const vuetify = createVuetify();
 
 const app = createApp({
-    el: "#app",
-    data: () => ({
-        items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-        question: 'What is your biggest regret?',
-        textarea: '',
-        email: '',
-        quizSection: 0,
-        ex3: 'primary',
+  el: "#app",
+  data: () => ({
+    question: 'What is your biggest regret?',
+    textarea: '',
+    email: '',
+    quizSection: 0,
+    sayNo: false,
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid',
+    ],
+    textareaRules: [
+      v => !!v || 'Answer is required',
+    ],
 
-    }),
-    computed: {
-        isTextarea () {
-           return !this.textarea
-        }
-    }
-      
+
+  }),
+
 })
 app.use(vuetify).mount('#app');
 
 
 class Writer {
-    constructor(node) {
-      this.node = node;
-      
-      if (!this.node) return;
-  
-      this.timer = 50; // .2s
-      this.broken = this.node.textContent.split('');
-  
-      this._init();
-    }
-  
-    _init() {
-      this.node.textContent = '';
-      let i = 0;
-  
-      let interval = setInterval(() => {
-        this.node.textContent += this.broken[i];
-  
-        i++;
-  
-        if (i >= this.broken.length) clearInterval(interval);
-      }, this.timer);
-    }
+  constructor(node) {
+    this.node = node;
+
+    if (!this.node) return;
+
+    this.timer = 50; // .2s
+    this.broken = this.node.textContent.split('');
+
+    this._init();
   }
-  
-  const textAnswer = document.querySelector('.textAnswer');
-  if(textAnswer){
-    new Writer(textAnswer);
+
+  _init() {
+    this.node.textContent = '';
+    let i = 0;
+
+    let interval = setInterval(() => {
+      this.node.textContent += this.broken[i];
+
+      i++;
+
+      if (i >= this.broken.length) clearInterval(interval);
+    }, this.timer);
   }
+}
+
+const textAnswer = document.querySelector('.textAnswer');
+const getAcquaintedText = document.querySelector('.getAcquaintedText');
+if (textAnswer) {
+  new Writer(textAnswer);
+}
+var AcquaintedTextVisible = 0;
+
+if (getAcquaintedText) {
+  getAcquaintedText.style.visibility = "hidden";
+  const callback = () => {
+    AcquaintedTextVisible = AcquaintedTextVisible + 1;
+    console.log("🚀 ~ file: main.js:66 ~ callback ~ AcquaintedTextVisible:", AcquaintedTextVisible)
+    
+    if(AcquaintedTextVisible === 2){
+      new Writer(getAcquaintedText);
+      getAcquaintedText.style.visibility = "visible";
+    }
+   
+  }
+  const options = {
+    threshold: 0.5 // 1 – полная видимость элемента, 0.5 – половина и т.д.
+  }
+  const observer = new IntersectionObserver(callback, options);
+
+  observer.observe(getAcquaintedText)
+}
+
+
